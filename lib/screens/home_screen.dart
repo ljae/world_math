a38a7805622952841b3b3ec0dfb020103bac5dd7
Jae Lee <jaelee@Jaes-MacBook-Air.local>
2025-12-11 23:23:12 +0900

    fix: Correct import path for foundation.dart

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,4 @@
-import 'package.flutter/foundation.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import '../services/firestore_service.dart';
