1f3cce67127f7d1d58391a6392c80781b660acaa
Jae Lee <jaelee@Jaes-MacBook-Air.local>
2025-12-13 09:52:46 +0900

    fix: Add kDebugMode to foundation import

--- lib/screens/problem_detail_screen.dart
+++ lib/screens/problem_detail_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/foundation.dart' show kIsWeb;
+import 'package:flutter/foundation.dart' show kIsWeb, kDebugMode;
 import 'package:flutter_markdown_plus/flutter_markdown_plus.dart';
 import 'package:flutter_math_fork/flutter_math.dart';
 import 'package:markdown/markdown.dart' as md;
